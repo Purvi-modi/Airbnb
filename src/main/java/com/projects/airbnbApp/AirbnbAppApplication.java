7326867cb3178884b7dc27bfa0aca12a2053d1f6
Purvi-modi <purvimodi1997@gmail.com>
2025-10-11 11:51:26 +0530

    initial commit

---
+++ src/main/java/com/projects/airbnbApp/AirbnbAppApplication.java
@@ -0,0 +1,14 @@
+package com.projects.airbnbApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbnbAppApplication.class, args);
+
+	}
+
+}
---
+++ src/main/java/com/projects/airbnbApp/entity/Hotel.java
@@ -0,0 +1,41 @@
+package com.projects.airbnbApp.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name="hotel")
+public class Hotel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String city;
+
+    @Column(columnDefinition = "TEXT[]")  // this can store something like s3 object url
+    private String[] photos;
+
+    @Column(columnDefinition = "TEXT[]")
+    private String[] amenities;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Embedded
+    private HotelContactInfo contactInfo;
+
+}
